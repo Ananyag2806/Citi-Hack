64ae7bb269193c0ddc5d5ffec4b9bedd1749f6cd
Ananya Gupta <ananyag2806@gmail.com>
2022-08-19 15:40:53 +0530

    demo

--- optimizer.py
+++ optimizer.py
@@ -7,7 +7,7 @@ import yfinance as yf
 st.set_page_config(page_title="optimizer",
                    page_icon="📈", layout="wide", initial_sidebar_state="auto")
 st.image("citi-logo-white.png", width=100)
-st.title("Citi Portfolio Optimizer 💲📈")
+st.title("Citi Portfolio Optimizer")
 
 # st.markdown(f"""
 #     <style>
